b78597cdd058610fcdd9b0e70bf1b2611c19316c
Leonid Stoyko <gribojor@gmail.com>
2026-03-04 18:18:03 +0500

    переделал под запрос
    def shorten_link(token, url):

--- main.py
+++ main.py
@@ -2,36 +2,37 @@ import requests
 import os
 import json
 from dotenv import load_dotenv
+from urllib.parse import urlsplit
 
 
 
-def get_short_url(headers):
-    url = 'https://clc.li/api/url/add'
+def shorten_link(token, url):
     link_request = input("Введите ссылку для сокращения:")
-    
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
     body = {
         "url": link_request
     }
     response = requests.post(url, headers=headers, json=body)
     response.raise_for_status()
-    return response
+    short_url = response.json()
+    split_link = urlsplit(short_url['shorturl'])
+
+    return (split_link.netloc+split_link.path)
 
 
 def main():
     load_dotenv()
-    api_clc_li = os.getenv('API_CLC_LI')
-    if not api_clc_li:
+    url = 'https://clc.li/api/url/add'
+    token = os.getenv('API_CLC_LI')
+    if not token:
         print("ОШИБКА: Не найден токен в переменных окружения!")
         return
     
-    headers = {
-        'Authorization': f'Bearer {api_clc_li}',
-        'Content-Type': 'application/json'
-    }
-    response = get_short_url(headers)
-    short_url = response.json()
-    
-    print("\nВот ваша ссылка:", short_url['shorturl'])
+    print('Короткая ссылка', shorten_link(token, url))
     # print(json.dumps(short_url, indent=4, ensure_ascii=False))
     # print(response)
     # print(response.text)
